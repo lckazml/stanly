3d887e75206a7da86aa9a08c7233603821f68403
lckazml <345031671@qq.com>
2017-11-27 12:58:57 +0800

    Stanley

--- www/resources/views/child.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('app')
-@section('title','page title')
-@section('sidebar')
-    @parent
-    <p>this is appended to the master sidebar</p>
-@endsection
-    @section('content')
-        <p>this is my body content.</p>
-@endsection
